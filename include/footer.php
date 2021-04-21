3a7adda7bc582ca587ec0e3eb28c93de24840bdc
sarveshpaul <55048116+sarveshpaul@users.noreply.github.com>
2021-04-21 08:20:57 +0530

    Add files via upload

---
+++ include/footer.php
@@ -0,0 +1,11 @@
+<link rel='stylesheet' type='text/css' media='screen' href='css/footer.css'>
+<center>
+    <div class="footer">
+        <span>
+            &copy; <?php echo date("Y")?> The Sparks Foundation || <label class="name">Developer by Sarvesh</label> 
+        </span>
+    </div>
+</center>
+<style>
+
+</style>
